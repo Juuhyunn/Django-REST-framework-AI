021564df1a64518ddbb7b47708c4b22b029011c2
Juuhyunn <ahnju96@gmail.com>
2021-09-23 19:16:30 +0900

    23, September

---
+++ admin/sorting/models.py
@@ -0,0 +1,71 @@
+from dataclasses import dataclass
+from builtins import property
+
+from django.db import models
+
+# Create your models here.
+
+
+class Sorting(object):
+    def bubble_sort(self):
+        pass
+
+    def merge_sort(self):
+        pass
+
+    def quick_sort(self):
+        pass
+
+@dataclass
+class Palindrome(object):
+    input_string : str
+
+    @property
+    def input_string(self) -> str: return self._input_string
+    @input_string.setter
+    def input_string(self, input_string): self._input_string = input_string
+
+
+    def str_to_list(self) -> []:
+        return [i.lower() for i in self.input_string if i.isalnum()]
+
+    def isPalindrome(self, ls : []) -> bool:
+        return [False for i in ls if ls.pop(0) != ls.pop()]
+
+    def reverse_string(self, ls : []) -> []:
+        # return ls[::-1]
+        return [ls.pop() for i in range(len(ls))]
+
+
+@dataclass
+class MySum(object):
+    start_number: int
+    end_number: int
+
+    @property
+    def start_number(self) -> int : return self._start_number
+
+    @start_number.setter
+    def start_number(self, start_number): self._start_number = start_number
+
+    @property
+    def end_number(self) -> int : return self._end_number
+
+    @end_number.setter
+    def end_number(self, end_number): self._end_number = end_number
+
+
+    # example 1
+    def one_to_ten_sum_1(self):
+        sum = 0
+        for i in range(self.start_number, self.end_number):
+            sum += i
+        return sum
+
+    # example 2
+    def one_to_ten_sum_2(self):
+        return sum(i for i in range(self.start_number, self.end_number))
+
+    # example 3
+    def one_to_ten_sum_3(self):
+        return sum(range(self.start_number, self.end_number))
---
+++ admin/sorting/tests.py
@@ -0,0 +1,38 @@
+from django.test import TestCase
+import unittest
+# Create your tests here.
+# import sys
+# sys.path.append('/admin/sorting')
+from models import MySum, Palindrome
+
+class TestMySum(unittest.TestCase):
+    def test_one_to_ten_sum_1(self):
+        instance = MySum()
+        instance.start_number = 1
+        instance.end_number = 11
+        res = instance.one_to_ten_sum_1()
+        print(f'My Expected Value is {res}')
+        self.assertEqual(res, 55)
+
+class TestPalindrome(unittest.TestCase):
+
+    def test_str_to_list(self):
+        instance = Palindrome()
+        instance.input_string = 'applE'
+        ls = instance.str_to_list()
+        print(f'My Expected Value is {ls}')
+        res = instance.isPalindrome(ls)
+        print(f'My Expected Value is {res}')
+        self.assertEqual(res, False)
+
+    def test_reverse_string(self):
+        instance = Palindrome()
+        instance.input_string = 'applE'
+        ls = instance.str_to_list()
+        print(f'My Expected Value is {ls}')
+        res = instance.reverse_string(ls)
+        print(f'My Expected Value is {res}')
+
+
+if __name__ == '__main__':
+    unittest.main()
